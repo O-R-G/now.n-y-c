909d5426116f8ef1bd2c5df2d15265d8e925541c
Wei-Hao Wang <wwang03@risd.edu>
2020-07-30 17:52:44 -0400

    remove json.stringify

--- index.js
+++ index.js
@@ -361,7 +361,7 @@ function shuffle(array) {
 // }
 function paste_msgs(){
 	msgs_temp = [msgs_sections['opening']];
-	console.log('sequence = '+sequence);
+	console.log('sequence.sq = '+sequence['sequence']);
 	for(i = 0; i < sequence['sequence'].length; i++){
 		var this_key = sequence['sequence'][i];
 		console.log(this_key);
