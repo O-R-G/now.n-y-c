db77c05f21a1bff777362e1cd20b063ca066273f
Wei-Hao Wang <wwang03@risd.edu>
2020-08-03 17:36:33 -0400

    fix cache time

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ msgs_sections['ending'] = ' 0 1 2 3 4 5 6 7 8 9 Have a nice day.';
 var msgs_break = '///';
 
 // 	only 47 chars cause one if left for the blinking block
-var msgs_beginning = 'NEW YORK CONSOLIDATED                          '; 
+var msgs_beginning = 'NEW YORK CONSOLIDATED                           '; 
 
 // preventing from the animation starts before data loaded.
 // if ready_now == 0 when an api is loaded, 
