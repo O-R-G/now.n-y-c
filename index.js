7440790c7a4f57dac17d1cb183e3b0b1407f60b6
Wei-Hao Wang <wwang03@risd.edu>
2021-07-02 13:50:49 +0800

    print path

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var app = express();
 app.use(cors());
+console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 var supported_lang = ['en', 'zh', 'es', 'ko', 'ar'];
 
